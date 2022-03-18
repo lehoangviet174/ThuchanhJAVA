1e6b38ca1b399919eea373e810c67b3f0c0e6123
lehoangviet174 <97715778+lehoangviet174@users.noreply.github.com>
2022-03-18 09:44:52 +0700

    Add files via upload

---
+++ BaiThucHanh1803/HinhTron.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class DienTichChuViHT {
+    public static double tinhDienTich(double r) {
+        return r * r * 3.14;
+    }
+
+    public static double tinhChuVi(double r) {
+        return r * 2 * 3.14;
+    }
+
+    public static void main(String[] args) {
+        double r;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Nhập bán kính hình tròn: ");
+       
+        r = scanner.nextDouble();
+       
+        System.out.println("Diện tích hình tròn là: " + tinhDienTich(r));
+        
+        System.out.println("Chu vi hình tròn là: "+ tinhChuVi(r));
+        
+        System.out.println("----------------------------");
+        
+        System.out.println("Chương trình nay được đăng tại Freetuts.net");
+    }
