842a9f598029ef363cd3bfaef93d6e82ff08c528
lehoangviet174 <97715778+lehoangviet174@users.noreply.github.com>
2022-04-15 23:48:52 +0700

    Add files via upload

---
+++ Baithuchanh15.04/src/App67.java
@@ -0,0 +1,12 @@
+import Shapes.HinhTron;
+import Shapes.HinhTru;
+
+public class App67 {
+    public static void main(String[] args) throws Exception {
+        HinhTron hinhTron = new HinhTron();
+        HinhTru hinhTru = new HinhTru();
+
+        hinhTron.xuatThongTin();
+        hinhTru.xuatThongTin();
+    }
+}
