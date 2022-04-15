48508f018273ae3c017b6f6a3b37eed87bf4e055
lehoangviet174 <97715778+lehoangviet174@users.noreply.github.com>
2022-04-15 10:41:27 +0700

    Add files via upload

---
+++ Baithuchanh15.04/src/App54.java
@@ -0,0 +1,45 @@
+import shapes.HinhChuNhat;
+import shapes.HinhTron;
+import shapes.HinhTru;
+import shapes.HinhVuong;
+
+public class App54 {
+    public static void main(String[] args) throws Exception {
+
+        // Hình tròn
+        HinhTron hinhtron = new HinhTron();
+        hinhtron.xuatTen();
+        hinhtron.nhapBanKinh();
+        hinhtron.tinhChuVi();
+        hinhtron.tinhDienTich();
+        hinhtron.inchuVi();
+        hinhtron.indienTich();
+
+        // Hinh trụ
+        /*
+         * HinhTru hinhtru = new HinhTru();
+         * hinhtru.nhapChieuCao();
+         * hinhtru.tinhTheTich();
+         * hinhtru.intheTich();
+         */
+        // Hình Chữ Nhật
+        /*
+         * HinhChuNhat hinhchunhat = new HinhChuNhat();
+         * hinhchunhat.nhapChieuDai();
+         * hinhchunhat.nhapChieuRong();
+         * hinhchunhat.tinhChuVi();
+         * hinhchunhat.tinhDienTich();
+         * hinhchunhat.inchuVi();
+         * hinhchunhat.indienTich();
+         */
+        // Hình Vuông
+        /*
+         * HinhVuong hinhvuong = new HinhVuong();
+         * hinhvuong.nhapCanh();
+         * hinhvuong.tinhChuVi();
+         * hinhvuong.tinhDienTich();
+         * hinhvuong.inchuVi();
+         * hinhvuong.indienTich();
+         */
+    }
+}
